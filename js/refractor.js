7281a7f8badf9c28114a606bece92171891cb85c
Kai <shadowkai0121@users.noreply.github.com>
2019-07-29 20:29:08 +0800

    人物重構 ok

--- js/refractor.js
+++ js/refractor.js
@@ -20,34 +20,14 @@ canvas.height = 480;
 
 let ctx = canvas.getContext('2d');
 
-// 繪圖程式
-function draw(obj) {
-    console.log(obj);
-    // ctx.drawImage(
-    //     obj.img,
-    //     // 人物擷取範圍
-    //     obj.avatarImg.x, obj.avatarImg.y,
-    //     obj.avatarImg.width, obj.avatarImg.height,
-    //     // 人物在地圖的位置
-    //     obj.x, obj.y,
-    //     // 人物在地圖的大小
-    //     obj.avatarImg.width * obj.scale, obj.avatarImg.height * obj.scale
-    // );
+let bg = new Image();
 
-    ctx.drawImage(
-        obj.img,
-        // 人物擷取範圍
-        0, 0,
-        32, 32,
-        // 人物在地圖的位置
-        obj.x, obj.y,
-        // 人物在地圖的大小
-        32, 32
-    );
-    console.log('done');
+bg.src = "img/bg.png";
+bg.onload = function () {
+    drawBackGround();
 }
 
-function drawBackGround(bg) {
+function drawBackGround() {
     reset();
     ctx.drawImage(bg,
         0, 0,
@@ -58,20 +38,22 @@ function reset() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-let bgReady = false,
-    bg = new Image();
-
-bg.src = "img/bg.png";
-bg.onload = function () {
-    drawBackGround(bg);
-    bgReady = true;
+// 繪圖程式
+function draw(obj) {
+    ctx.drawImage(obj.img,
+        // 人物擷取範圍
+        obj.avatarImg.x, obj.avatarImg.y,
+        obj.avatarImg.width, obj.avatarImg.height,
+        // 人物在地圖的位置
+        obj.x, obj.y,
+        //     // 人物在地圖的大小
+        obj.avatarImg.width, obj.avatarImg.height);
 }
-
-
 
 /*******************************************/
 // 物件
 
+// 方向表
 let direction = [
     "right", "left", "top", "bottom",
     "topRight", "topLeft", "bottomRight", "bottomLeft"
@@ -101,12 +83,28 @@ MapObject.prototype = {
     move: function () {
         switch (this.direction) {
             case "right":
+                this.x += this.dx;
+                if (this.x >= canvas.width) {
+                    this.x = canvas.width;
+                }
                 break;
             case "left":
+                this.x -= this.dx;
+                if (this.x <= 0) {
+                    this.x = 0;
+                }
                 break;
             case "top":
+                this.y -= this.dy;
+                if (this.y <= 0) {
+                    this.y = 0;
+                }
                 break;
             case "bottom":
+                this.y += this.dy;
+                if (this.y >= canvas.height) {
+                    this.y = canvas.height;
+                }
                 break;
             case "topRight":
                 break;
@@ -134,26 +132,81 @@ function Warrior() {
 Warrior.prototype = Object.create(MapObject.prototype);
 
 Warrior.prototype.action = function (obj) {
+    // 圖片 X 軸切割，每一次執行變更一個寬度的距離
     obj.avatarImg.x += obj.actionCounter * obj.avatarImg.width;
-    if (obj.avatarImg.x >= obj.img.width - obj.action.width ||
+
+    if (obj.avatarImg.x >= obj.img.width - obj.avatarImg.width ||
         obj.avatarImg.x <= 0) {
+        // 反轉切割方向
         obj.actionCounter *= -1;
     }
-    switch (obj.direction) {
+
+    setTimeout(obj.action, 1000 / 5, obj);
+}
+
+Warrior.prototype.trunArround = function (direct) {
+    // 設置角色當前方向
+    this.direction = direct;
+
+    // 因應角色方向選擇切割圖片的 Y 軸
+    switch (this.direction) {
         case "right":
-            obj.avatarImg.y = obj.avatarImg.height * 2;
+            this.avatarImg.y = this.avatarImg.height * 2;
             break;
         case "left":
-            obj.avatarImg.y = obj.avatarImg.height * 1;
+            this.avatarImg.y = this.avatarImg.height * 1;
             break;
         case "top":
-            obj.avatarImg.y = obj.avatarImg.height * 3;
+            this.avatarImg.y = this.avatarImg.height * 3;
             break;
         case "bottom":
-            obj.avatarImg.y = obj.avatarImg.height * 0;
+            this.avatarImg.y = this.avatarImg.height * 0;
             break;
     }
 }
 
-let war = new Warrior();
-draw(war);
+// 玩家物件
+let player = new Warrior();
+
+// 使用者功能
+let keysdown = [];
+document.onkeydown = function (e) {
+    if ([37, 38, 39, 40].includes(e.keyCode)) {
+        e.preventDefault();
+    }
+    keysdown[e.keyCode] = true;
+}
+
+document.onkeyup = function (e) {
+    delete keysdown[e.keyCode];
+}
+
+
+function main() {
+
+    // 判斷按下的按鍵
+    if (37 in keysdown) {
+        player.trunArround("left");
+        player.move();
+    }
+    if (38 in keysdown) {
+        player.trunArround("top");
+        player.move();
+    }
+    if (39 in keysdown) {
+        player.trunArround("right");
+        player.move();
+        draw(player);
+    }
+    if (40 in keysdown) {
+        player.trunArround("bottom");
+        player.move();
+    }
+
+    // 繪製物件
+    drawBackGround();
+    draw(player);
+
+    requestAnimationFrame(main);
+}
+main();
