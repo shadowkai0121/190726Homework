2c139a306d54c4e0b98fb1b45dda8fcad82bf982
shadowkai0121 <shadowkai0121@gmail.com>
2019-07-30 13:35:37 +0800

    技能物件重構OK

--- js/refractor.js
+++ js/refractor.js
@@ -1,3 +1,12 @@
+/*
+    todo{
+        改寫 Explosion 建構子
+        改寫 Explosion 物件顯示
+        在 Warrior 施放 Piercing 時產生 Explosion
+        修正原本 Boss 被攻擊時 Explosion 顯示位置錯誤 Bug
+    }
+
+*/
 // 複製物件
 function clone(source) {
     if (source == null || typeof (source) != 'object') return null;
@@ -40,8 +49,6 @@ function reset() {
 
 // 繪圖程式
 function draw(obj) {
-    if (obj instanceof Piercing) {
-    }
 
     ctx.drawImage(obj.img,
         // 人物擷取範圍
@@ -65,8 +72,6 @@ let avatarImg = {
     height: 32,
 }
 
-
-
 // 地圖物件類別
 function MapObject() {
     this.x = Math.random() * canvas.width;
@@ -79,14 +84,7 @@ function MapObject() {
 }
 
 MapObject.prototype = {
-    setDirection: function () {
-        let direction = [
-            "right", "left", "top", "bottom",
-            "topRight", "topLeft", "bottomRight", "bottomLeft"
-        ];
-
-        this.direction = direction[parseInt(Math.random() * direction.length)];
-    },
+    // 更新人物當前的作用範圍
     setScope: function () {
         this.scope = {
             minX: this.x,
@@ -97,6 +95,7 @@ MapObject.prototype = {
     },
     move: function () {
 
+        // 依據方向變更圖片位置
         switch (this.direction) {
             case "right":
                 this.x += this.dx;
@@ -128,6 +127,7 @@ MapObject.prototype = {
                 break;
         }
 
+        // 判斷是否觸碰到地圖邊界
         if (this.x >= canvas.width - this.avatarImg.width) {
             this.x = canvas.width - this.avatarImg.width;
         }
@@ -144,6 +144,16 @@ MapObject.prototype = {
         // 每次移動時更新物件實體範圍
         this.setScope();
     },
+    // 變更方向
+    setDirection: function () {
+        let direction = [
+            "right", "left", "top", "bottom",
+            "topRight", "topLeft", "bottomRight", "bottomLeft"
+        ];
+
+        this.direction = direction[parseInt(Math.random() * direction.length)];
+    },
+    // 讓 NPC 亂跑
     randomWalk: function (obj) {
         obj.setDirection();
         setTimeout(obj.randomWalk, Math.random() * (1.5 * 1000), obj);
@@ -152,6 +162,7 @@ MapObject.prototype = {
 
 
 
+// 玩家建構子
 function Warrior() {
     MapObject.call(this);
     this.x = 50;
@@ -159,27 +170,30 @@ function Warrior() {
     this.direction = "right";
     this.img = new Image();
     this.img.src = "img/warrior.png";
-    this.actionCounter = 1;
+    // 控制當前人物動作
+    this.actionController = 1;
     this.setScope();
     this.action(this);
 }
 
 Warrior.prototype = Object.create(MapObject.prototype);
 
+// 動作動畫
 Warrior.prototype.action = function (obj) {
     // 圖片 X 軸切割，每一次執行變更一個寬度的距離
-    obj.avatarImg.x += obj.actionCounter * obj.avatarImg.width;
+    obj.avatarImg.x += obj.actionController * obj.avatarImg.width;
 
     if (obj.avatarImg.x >= obj.img.width - obj.avatarImg.width ||
         obj.avatarImg.x <= 0) {
         // 反轉切割方向
-        obj.actionCounter *= -1;
+        obj.actionController *= -1;
     }
 
     setTimeout(obj.action, 1000 / 5, obj);
 }
 
-Warrior.prototype.trunArround = function (direct) {
+// 改變方向
+Warrior.prototype.setDirection = function (direct) {
     // 設置角色當前方向
     this.direction = direct;
 
@@ -200,6 +214,12 @@ Warrior.prototype.trunArround = function (direct) {
     }
 }
 
+Warrior.prototype.piercing = function () {
+    let piercing = new Piercing(this);
+    piercing.isHit();
+    skillObjs.push(piercing);
+}
+
 
 
 function Brick() {
@@ -217,6 +237,7 @@ Brick.prototype = Object.create(MapObject.prototype);
 Brick.prototype.move = function () {
     MapObject.prototype.move.call(this);
 
+    // 撞到邊邊的時候換隨機方向
     if (
         this.x >= canvas.width - this.avatarImg.width ||
         this.y >= canvas.height - this.avatarImg.height ||
@@ -228,24 +249,25 @@ Brick.prototype.move = function () {
 }
 
 
-let piercingImg = {
+// 圖片規格
+let attachSkillImg = {
     scale: 0.5,
     x: 0,
     y: 0,
-    width: 320,
-    height: 240
+    width: 320, height: 240
 }
 
 function Piercing(caster) {
 
-    this.avatarImg = clone(piercingImg);
+    this.avatarImg = clone(attachSkillImg);
 
     switch (caster.direction) {
         case "right":
+            // 校正初始位置使技能效果能從人物中線發射
             this.x = caster.x + caster.avatarImg.width;
             this.y = caster.y - this.avatarImg.height * this.avatarImg.scale / 2 + caster.avatarImg.height / 2;
-            console.log(`this.y = ${caster.y} - ${this.avatarImg.height} * ${this.avatarImg.scale} / 2 + ${caster.avatarImg.height} / 2`);
-            // 作用範圍
+
+            // 設置作用範圍
             this.scope = {
                 minX: caster.x + caster.avatarImg.width,
                 minY: caster.y,
@@ -288,27 +310,153 @@ function Piercing(caster) {
             break;
     }
 
-    this.actionCounter = 0;
+    this.direction = caster.direction;
+    this.actionController = 0;
     this.img = new Image();
     // 載入對應方向的圖片
     this.img.src = "img/piercing_" + caster.direction + ".png";
-    console.log(`piercing(x, y) = ${this.x}, ${this.y}`);
+    this.action(this);
+}
+
+Piercing.prototype.action = function (obj) {
+    // 取得下一張圖位置
+    let col = obj.actionController % (obj.img.width / obj.avatarImg.width),
+        row = Math.floor(obj.actionController / (obj.img.height / obj.avatarImg.height));
+
+    // 動態調整裁切範圍
+    obj.avatarImg.x = obj.avatarImg.width * col;
+    obj.avatarImg.y = obj.avatarImg.height * row;
+
+    // 計算目前執行次數
+    obj.actionController++;
+    if (obj.actionController >= 9) {
+        // 標記執行完成
+        obj.isDelete = true;
+        return;
+    }
+
+    setTimeout(obj.action, 1000 / 120, obj);
+}
+
+Piercing.prototype.isHit = function () {
+    for (let enemyObj of enemyObjs) {
+        switch (this.direction) {
+            case "right":
+                /*                                 
+                    enemy.minX  enemy.maxX      
+                        |          |                
+                        +----------+ enemy.minY    
+                        |   enemy  |               
+                        |          |            skill.maxX
+                    +------------------------------+ skill.minY
+                    |   |          |               |
+                    |   +----------+ enemy.maxY    |
+                    |                              |
+                    +------------------------------+ skill.maxY
+                                +---------------------->
+                */
+                if (
+                    this.scope.maxX >= enemyObj.scope.minX &&
+                    this.scope.minX <= enemyObj.scope.minX &&
+                    this.scope.minY <= enemyObj.scope.maxY ||
+                    this.scope.maxY >= enemyObj.scope.minY
+                ) {
+                    console.log("right hit");
+                }
+                break;
+            case "left":
+                /*
+                                    enemy.minX   enemy.maxX
+                                       +----------+  skill.maxX
+                                       |          |    +
+                  skill.minX           |          |    |
+                        +------------------------------+ skill.minY
+                        |              |          |    |
+                        |   enemy.maxY +----------+    |
+                        |                              |
+                        +------------------------------+ skill.maxY
+                    <-------------------+
+
+                 */
+                if (this.scope.minX <= enemyObj.scope.minX &&
+                    this.scope.maxX >= enemyObj.scope.maxX &&
+                    this.scope.minY >= enemyObj.scope.maxY ||
+                    this.scope.maxY >= enemyObj.scope.minY) {
+                    console.log("left hit");
+                }
+                break;
+            case "top":
+                /*
+                                              ^
+                                              |
+                      skill.minY+-----------+ |
+                                |           | |
+                                |           | |
+                enemy.minY+---------+       | +
+                          |     |   |       |
+                          |     |   |       |
+                          |     |   |       |
+                          |     |   |       |
+                enemy.maxY+---------+       |
+                    enemy.minX  | enemy.maxX|
+                                |           |
+                                |           |
+                                +-----------+ skill.maxY
+                        skill.minX     skill.maxX
+                */
+                if (
+                    this.scope.minY <= enemyObj.scope.minY &&
+                    this.scope.maxY >= enemyObj.scale.minY &&
+                    this.scope.minX <= enemyObj.scope.maxX ||
+                    this.scope.maxX >= enemyObj.scope.minX
+                ) {
+                    console.log("top hit");
+                }
+                break;
+            case "bottom":
+                /*
+                    skill.minX  skill.maxX
+                          +---------+ skill.minY
+                          |         |
+                          |         |
+         enemy.minY +----------+    |
+                    |     |    |    |
+                    |     |    |    |  +
+                    |     |    |    |  |
+                    |     |    |    |  |
+         enemy.maxY +----------+    |  |
+             enemy.minX   |    |    |  |
+                          |    |    |  |
+                          |    |    |  |
+               skill.maxY +---------+  |
+                               |       |
+                               +       v
+                        enemy.maxX
+                */
+                if (
+                    this.scope.maxY >= enemyObj.scope.maxY &&
+                    this.scope.minY <= enemyObj.scope.maxY &&
+                    this.scope.minX <= enemyObj.scope.maxX ||
+                    this.scope.maxX >= enemyObj.scope.minX
+                ) {
+                    console.log("bottom hit");
+                }
+                break;
+        }
+    }
 }
 
 // 物件管理中心
-let enemyObj = [],
-    skillObj = [];
+let enemyObjs = [],
+    skillObjs = [];
 
 // 玩家物件
 let player = new Warrior();
 // Boss
 let boss = new Brick();
-enemyObj.push(boss);
+enemyObjs.push(boss);
 
-let piercing = new Piercing(player);
-
-
-// 使用者功能
+// 紀錄按鍵資訊 
 let keysdown = [];
 document.onkeydown = function (e) {
     if ([37, 38, 39, 40].includes(e.keyCode)) {
@@ -321,34 +469,48 @@ document.onkeyup = function (e) {
     delete keysdown[e.keyCode];
 }
 
-
+// 遊戲迴圈
 function main() {
 
     // 判斷按下的按鍵
     if (37 in keysdown) {
-        player.trunArround("left");
+        player.setDirection("left");
         player.move();
     }
     if (38 in keysdown) {
-        player.trunArround("top");
+        player.setDirection("top");
         player.move();
     }
     if (39 in keysdown) {
-        player.trunArround("right");
+        player.setDirection("right");
         player.move();
         draw(player);
     }
     if (40 in keysdown) {
-        player.trunArround("bottom");
+        player.setDirection("bottom");
         player.move();
     }
-    // boss.move();
+    if (90 in keysdown) {
+        player.piercing();
+    }
 
+    // boss.move();
     // 繪製物件
     drawBackGround();
     draw(player);
     draw(boss);
-    draw(piercing);
+
+    // 清除已刪除物件
+    skillObjs = skillObjs.filter(obj => {
+        return obj.isDelete != true;
+    });
+
+    // 畫出所有技能物件
+    for (let skillObj of skillObjs) {
+        if (skillObj) {
+            draw(skillObj);
+        }
+    }
 
     requestAnimationFrame(main);
 }
